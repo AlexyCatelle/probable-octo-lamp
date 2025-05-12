758ed29244422dbf46a1017bc57c2b91893ac0e3
AlexyCatelle <alexy.catelle@oclock.school>
2025-05-12 16:59:02 +0200

    exo 8

--- 09-PHP/exercices/4_itératives/Exercice8.php
+++ 09-PHP/exercices/4_itératives/Exercice8.php
@@ -3,3 +3,15 @@
 # Exercice 8 : while
 
 #Calculez la somme des nombres pairs compris entre 0 et 100 en utilisant une boucle while .
+$sum = 0;
+$i=0;
+
+while ($i <= 100){
+if ($i%2 === 0){
+    $sum= $sum + $i;
+    echo "$i est pair. Le nouveau total est $sum .", PHP_EOL;
+}
+$i++;
+};
+
+
