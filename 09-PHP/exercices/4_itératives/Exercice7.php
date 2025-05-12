c8378a8eda09fcf17f842ed054a33b8b3a81d5ed
AlexyCatelle <alexy.catelle@oclock.school>
2025-05-12 16:49:32 +0200

    exo 7

---
+++ 09-PHP/exercices/4_itératives/Exercice7.php
@@ -0,0 +1,21 @@
+<?php
+
+# Exercice 7 : for
+
+//Demandez à l'utilisateur d'entrer un nombre.
+
+$number = (int)readline("Entrez un nombre : ");
+// Utilisez la boucle `for` pour parcourir tous les nombres de 1 à `N`.
+
+for ($i = 0; $i<= $number; $i++){
+    // Si le nombre est un multiple de 3 ou de 5, affichez-le. 
+if($i%3 === 0 ){
+    echo " $i est un multiple de 3.", PHP_EOL;
+}
+else if($i%5 === 0){
+    echo " $i est un multiple de 5.", PHP_EOL; 
+}
+else{
+echo PHP_EOL;
+};
+};
