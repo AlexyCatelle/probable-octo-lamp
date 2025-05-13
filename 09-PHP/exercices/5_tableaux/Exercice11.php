ed4cee825174eee8cedfeab29e4eaf677126ca1e
AlexyCatelle <alexy.catelle@oclock.school>
2025-05-13 12:16:20 +0200

    exo 11 tableau

--- 09-PHP/exercices/5_tableaux/Exercice11.php
+++ 09-PHP/exercices/5_tableaux/Exercice11.php
@@ -1,5 +1,37 @@
-## Exercice 11 : tableaux et parcours de chaine
+<?php
 
-- Créez un tableau contenant quelques prénoms.
-- Parcourez le tableau avec une boucle foreach et affichez chaque prénom à l'écran.
-- Ajoutez un test pour vérifier si le prénom contient la lettre "a" et affichez un message supplémentaire si c'est le cas.
+# Exercice 11 : tableaux et parcours de chaine
+
+//Créez un tableau contenant quelques prénoms.
+
+$nameList = [
+"Buck",
+"Eddie",
+"Hen",
+"Karen",
+"Chimney",
+"Maddie",
+"Bobby",
+"Athena",
+"Ravi",
+"Chris",
+"Harry",
+"May",
+"Mara",
+"Denny"
+];
+
+// Parcourez le tableau avec une boucle foreach et affichez chaque prénom à l'écran.
+
+foreach ($nameList as $name){
+echo $name, PHP_EOL;
+};
+
+//Ajoutez un test pour vérifier si le prénom contient la lettre "a" et affichez un message supplémentaire si c'est le cas.
+foreach ($nameList as $name){
+
+    if (stripos($name, 'a') !== false) {
+        echo "→ $name contient la lettre 'a'." . PHP_EOL;
+    };
+
+    };
