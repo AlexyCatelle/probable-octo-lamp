a2bc1b18eebc954e0e45abba2c5907b2bc36036e
AlexyCatelle <alexy.catelle@oclock.school>
2025-05-13 16:44:32 +0200

    exo 15 en cours

--- 09-PHP/exercices/6_fonctions/Exercice15.php
+++ 09-PHP/exercices/6_fonctions/Exercice15.php
@@ -1,7 +1,35 @@
-## Exercice 15 : fonctions anonymes
+<?php
 
-- Créer une fonction `doublerValeur` qui prend en paramètre un tableau d'entiers.
-- À l'intérieur de cette fonction, créez une fonction anonyme qui prendra un entier en paramètre et le multipliera par 2.
-- Utilisez cette fonction anonyme pour doubler chaque valeur contenue dans le tableau passé en argument.
-- La fonction `doublerValeur` devra retourner un nouveau tableau contenant les valeurs doublées.
-- Affichez les valeurs du tableau retourné. 
+# Exercice 15 : fonctions anonymes
+
+// Créer une fonction `doublerValeur` qui prend en paramètre un tableau d'entiers.
+
+function doublerValeur (int...$entiers) : void {
+
+    $doubleArray = [];
+
+  //À l'intérieur de cette fonction, créez une fonction anonyme qui prendra un entier en paramètre et le multipliera par 2.
+    $doublage = function($a){
+        return $a*2;
+    };
+  
+    // Utilisez cette fonction anonyme pour doubler chaque valeur contenue dans le tableau passé en argument.
+
+    foreach ($entiers as $entier){
+$resultat =$doublage($entier);
+
+      $doubleArray= $resultat ;  
+    };
+
+    echo $doubleArray;
+};
+
+
+
+// La fonction `doublerValeur` devra retourner un nouveau tableau contenant les valeurs doublées.
+
+//Affichez les valeurs du tableau retourné. 
+
+$dataArray = [118,911,8,9,11,29];
+
+doublerValeur(...$dataArray);
