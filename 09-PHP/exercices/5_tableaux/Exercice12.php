617266ee0aabcc135df7a5a230f71abff489ee8d
AlexyCatelle <alexy.catelle@oclock.school>
2025-05-13 12:39:55 +0200

    exercice 12

--- 09-PHP/exercices/5_tableaux/Exercice12.php
+++ 09-PHP/exercices/5_tableaux/Exercice12.php
@@ -1,5 +1,38 @@
-## Exercice 12 : tableaux et boucle while
+<?php
 
-- Demandez à l'utilisateur de saisir une note ou de taper "fin". 
-- Chaque note sera sauvegardé dans un tableau. 
-- Quand l'utilisateur tapera "fin", affichez l'ensemble des notes du tableau. 
+# Exercice 12 : tableaux et boucle while
+
+
+
+ 
+
+ $notesArray = [];
+
+do{
+    //Demandez à l'utilisateur de saisir une note ou de taper "fin".
+    $userInput = readline("Veuillez entrer une note ou 'fin' : "); 
+
+    // Chaque note sera sauvegardé dans un tableau.
+    if(is_numeric($userInput))
+    {
+        echo "la note est $userInput",PHP_EOL;
+        $notesArray[] = $userInput;
+
+    }
+
+    // Quand l'utilisateur tapera "fin", affichez l'ensemble des notes du tableau.
+    elseif ($userInput === 'fin'){
+        
+        echo "l'ensemble des notes est : ";
+        foreach($notesArray as $note){
+            echo "\n$note";
+            };
+    break;
+    }
+
+    else{
+    echo "Erreur de saisie", PHP_EOL;
+    };
+
+}while(true); 
+
